e1600683274cd96c6d9bd149d56b95577c025160
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-11-28 13:23:09 -0700

    remove debounce function

--- flask_meld/static/js/meld.js
+++ flask_meld/static/js/meld.js
@@ -235,35 +235,6 @@ function $(selector, scope) {
   return scope.querySelector(selector);
 }
 
-/*
-    Returns a function, that, as long as it continues to be invoked, will not
-    be triggered. The function will be called after it stops being called for
-    N milliseconds. If `immediate` is passed, trigger the function on the
-    leading edge, instead of the trailing.
-
-    Derived from underscore.js's implementation in https://davidwalsh.name/javascript-debounce-function.
-    */
-function debounce(func, wait, immediate) {
-  var timeout;
-
-  if (typeof immediate == undefined) {
-    immediate = true;
-  }
-
-  return function () {
-    var context = this, args = arguments;
-    var later = function () {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-
-    if (callNow) func.apply(context, args);
-  };
-};
 
 /*
     The function is executed the number of times it is called,
