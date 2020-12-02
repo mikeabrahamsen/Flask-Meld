263b6f36d6396d852eb4ad4997d12ffde0b4b091
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-12-01 17:14:00 -0700

    update setup.py description

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ Flask-Meld
 --------------
 A way to meld your frontend and backend code
 """
+import pathlib
 import re
 from setuptools import setup
 
 with open('flask_meld/__init__.py', 'r') as f:
     version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                         f.read(), re.MULTILINE).group(1)
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='Flask-Meld',
@@ -17,8 +21,9 @@ setup(
     license='MIT',
     author='Michael Abrahamsen',
     author_email='mail@michaelabrahamsen.com',
-    description='Meld your Flask applications',
-    long_description=__doc__,
+    description='Flask-Meld is a framework for Flask that allows you to create dynamic user interfaces using Python and the Jinja2 templating engine.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=['flask_meld'],
     zip_safe=False,
     include_package_data=True,
