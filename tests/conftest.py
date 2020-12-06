373c48d245412e21c629a556bc30020b23f6a6c6
Michael Abrahamsen <mail@michaelabrahamsen.com>
2020-12-05 18:38:12 -0700

    test that module is imported for different app structures
    - test project/app/meld/components
    - test project/meld/components

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,27 @@ from flask_meld import Meld
 
 @pytest.fixture(scope='module')
 def app(tmpdir_factory):
+    # create directory structure of project/meld/components
+    app_dir = tmpdir_factory.mktemp('meld')
+    app_dir.mkdir('components')
+    module = app_dir.join("search.py")
+    with module.open('w') as f:
+        class_def = ["from flask_meld.component import Component",
+                     "class Search(Component):",
+                     "\tstate=''"]
+        f.writelines(f"{line}\n" for line in class_def)
+
+    meld = Meld()
+    app = Flask(__name__)
+    app.config["MELD_COMPONENT_DIR"] = app_dir
+    app.secret_key = __name__
+    meld.init_app(app)
+    return app
+
+
+@pytest.fixture(scope='module')
+def app_factory(tmpdir_factory):
+    # create directory structure of project/app/meld/components
     app_dir = tmpdir_factory.mktemp('app')
     app_dir = app_dir.mkdir('meld').mkdir('components')
     module = app_dir.join("search.py")
@@ -24,8 +45,14 @@ def app(tmpdir_factory):
 
 
 @pytest.fixture
-def client(app):
-    return app.test_client()
+def client(app_factory):
+    return app_factory.test_client()
+
+
+@pytest.yield_fixture
+def app_factory_ctx(app_factory):
+    with app_factory.app_context() as ctx:
+        yield ctx
 
 
 @pytest.yield_fixture
--- tests/test_meld.py
+++ tests/test_meld.py
@@ -1,6 +1,11 @@
 from flask_meld.component import get_component_class
 
 
-def test_module_load_with_app_factory(app_ctx):
+def test_module_load_with_app_factory(app_factory_ctx):
+    component_class = get_component_class("search")
+    assert component_class.__name__ == "Search"
+
+
+def test_module_load_with_single_file_app(app_ctx):
     component_class = get_component_class("search")
     assert component_class.__name__ == "Search"
